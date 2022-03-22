26e620a976701b78bbe6a22e28641403a5e5619f
andrea.dattola <andre.d.96@hotmail.it>
2022-03-22 17:36:32 +0100

    fix: add custom hook for login and heder tab

---
+++ utils/isLoggedIn.js
@@ -0,0 +1,23 @@
+import React from "react";
+import react from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { LOGIN } from "../redux/actions/login";
+import {getCookieValue} from "./getCookie";
+
+const useLogin = () => {
+  const isLoggedIn = useSelector((state) => state.login);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    if (isLoggedIn.success) return;
+    const cookie = getCookieValue("etherLogin");
+    console.log('cookie', cookie)
+    if (!cookie) window.location.href = "/login";
+    if (JSON.parse(cookie).id)
+    dispatch({
+        type: LOGIN._REQUEST,
+        data: { _id: JSON.parse(cookie).id },
+      });
+  }, []);
+  return isLoggedIn;
+};
+export default useLogin;
